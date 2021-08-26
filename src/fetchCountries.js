3d89d720a78d0a7cba12d3b35fa5943d540c8db6
Bellik888 <Bellik888@gmail.com>
2021-08-26 17:52:46 +0300

    add parcel

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+function fetchCountries(name) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then((response) => {return response.json()});
+}
+
+export default fetchCountries;
