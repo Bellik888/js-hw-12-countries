30ba208fab05be4a3ddf2c30c30dfebb4b6aa3c3
Bellik888 <Bellik888@gmail.com>
2021-08-26 22:43:52 +0300

    add changes in html and js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import debounce from 'lodash.debounce';
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import fetchCountries from './fetchCountries';
+import countryTemplate from './templates/countryTemplate.hbs';
 
+
+const refs = {
+    input: document.querySelector('#input'),
+    countryList: document.querySelector('.country-list'),
+}
+
+input.addEventListener('input', debounce(searchCountry, 500));
+
+
+
+function searchCountry() {
+    let value = input.value;
+    
+    fetchCountries(value)
+        .then(data => updateTemplate(data))
+        .catch(error => console.log(error));
+    
+}
+
+function updateTemplate(data) {
+    const markup = countryTemplate(data);
+
+    refs.countryList.insertAdjacentHTML('beforeend', markup);
+}
