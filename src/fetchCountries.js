33e9f7c96bcd8df931604eff94ba61ae599991fb
Bellik888 <Bellik888@gmail.com>
2021-08-26 18:03:54 +0300

    delete old

--- src/fetchCountries.js
+++
@@ -1,6 +0,0 @@
-function fetchCountries(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-        .then((response) => {return response.json()});
-}
-
-export default fetchCountries;
--- src/index.js
+++
@@ -1,1 +0,0 @@
-import fetchCountries from './fetchCountries';
